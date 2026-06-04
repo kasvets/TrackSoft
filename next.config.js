5328cb360d7244a29af683280759ce90a51d3c4e
kasvets <sezerozger@yandex.com>
2026-06-04 14:09:07 +0300

    fix: add eslintrc and fix next.config to resolve vercel deployment error

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
+
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
   },
